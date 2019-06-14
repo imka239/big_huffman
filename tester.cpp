8a5ae02dc3da7cfba2067a677f34839969642eea
imka239 <ima2ima3go9@gmail.com>
2019-06-14 22:10:36 +0300

    asdasdasd

--- tester.cpp
+++ tester.cpp
@@ -44,50 +44,50 @@ void code_decode(const std::string &t1, const std::string &t2, const std::string
 }
 
 TEST(corner_case, only_aaaaaaaaa) {
-    std::string t1 = "test1/test_in.txt";
-    std::string t2 = "test1/test_coded.bin";
-    std::string t3 = "test1/test_out.txt";
+    std::string t1 = "../test1/test_in.txt";
+    std::string t2 = "../test1/test_coded.bin";
+    std::string t3 = "../test1/test_out.txt";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(corner_case, empty) {
-    std::string t1 = "test2/test_in.txt";
-    std::string t2 = "test2/test_coded.bin";
-    std::string t3 = "test2/test_out.txt";
+    std::string t1 = "../test2/test_in.txt";
+    std::string t2 = "../test2/test_coded.bin";
+    std::string t3 = "../test2/test_out.txt";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(corner_case, only11111111) {
-    std::string t1 = "test3/test_in.txt";
-    std::string t2 = "test3/test_coded.bin";
-    std::string t3 = "test3/test_out.txt";
+    std::string t1 = "../test3/test_in.txt";
+    std::string t2 = "../test3/test_coded.bin";
+    std::string t3 = "../test3/test_out.txt";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(corner_case, only00000000) {
-    std::string t1 = "test4/test_in.txt";
-    std::string t2 = "test4/test_coded.bin";
-    std::string t3 = "test4/test_out.txt";
+    std::string t1 = "../test4/test_in.txt";
+    std::string t2 = "../test4/test_coded.bin";
+    std::string t3 = "../test4/test_out.txt";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(corner_case, all_bytes) {
-    std::string t1 = "test5/test_in.txt";
-    std::string t2 = "test5/test_coded.bin";
-    std::string t3 = "test5/test_out.txt";
+    std::string t1 = "../test5/test_in.txt";
+    std::string t2 = "../test5/test_coded.bin";
+    std::string t3 = "../test5/test_out.txt";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(double_coding, war_and_peace) {
-    std::string t1 = "test6/test_in.txt";
-    std::string t2 = "test6/test_coded.bin";
-    std::string t4 = "test6/test_coded2.bin";
-    std::string t5 = "test6/test_out.txt";
+    std::string t1 = "../test6/test_in.txt";
+    std::string t2 = "../test6/test_coded.bin";
+    std::string t4 = "../test6/test_coded2.bin";
+    std::string t5 = "../test6/test_out.txt";
     huffman::file_handler file1(t1);
     file1.code(t2);
     huffman::file_handler file2(t2);
@@ -100,26 +100,26 @@ TEST(double_coding, war_and_peace) {
 }
 
 TEST(big_test, war_and_peace_full) {
-    std::string t1 = "test7/test_in.txt";
-    std::string t2 = "test7/test_coded.bin";
-    std::string t3 = "test7/test_out.txt";
+    std::string t1 = "../test7/test_in.txt";
+    std::string t2 = "../test7/test_coded.bin";
+    std::string t3 = "../test7/test_out.txt";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(big_test, just_big_text) {
-    std::string t1 = "test8/test_in.txt";
-    std::string t2 = "test8/test_coded.bin";
-    std::string t3 = "test8/test_out.txt";
+    std::string t1 = "../test8/test_in.txt";
+    std::string t2 = "../test8/test_coded.bin";
+    std::string t3 = "../test8/test_out.txt";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(four_code_decode_test, war_and_peace_full) {
-    std::string t1 = "test9/test_in.txt";
-    std::string t2 = "test9/test_coded.bin";
-    std::string t4 = "test9/test_coded2.bin";
-    std::string t5 = "test9/test_out.txt";
+    std::string t1 = "../test9/test_in.txt";
+    std::string t2 = "../test9/test_coded.bin";
+    std::string t4 = "../test9/test_coded2.bin";
+    std::string t5 = "../test9/test_out.txt";
     huffman::file_handler file1(t1);
     file1.code(t2);
     huffman::file_handler file2(t2);
@@ -141,37 +141,37 @@ TEST(four_code_decode_test, war_and_peace_full) {
 
 
 TEST(music, king_of_the_jungle) {
-    std::string t1 = "test10/Ricardo.mp3";
-    std::string t2 = "test10/test_coded.bin";
-    std::string t3 = "test10/music_uncode.mp3";
+    std::string t1 = "../test10/Ricardo.mp3";
+    std::string t2 = "../test10/test_coded.bin";
+    std::string t3 = "../test10/music_uncode.mp3";
     code_decode(t1, t2, t3);
     EXPECT_TRUE(FileEquals(t1, t3));
 }
 
 TEST(exception, war_and_peace_without_one_byte) {
-    std::string t1 = "test11/test_in.bin";
-    std::string t2 = "test11/test_out.txt";
+    std::string t1 = "../test11/test_in.bin";
+    std::string t2 = "../test11/test_out.txt";
     huffman::file_handler file1(t1);
     EXPECT_ANY_THROW(file1.decode(t2));
 }
 
 TEST(exception, no_file_out) {
-    std::string t1 = "test12/test_in.bin";
-    std::string t2 = "test12/test_out.txt";
+    std::string t1 = "../test12/test_in.bin";
+    std::string t2 = "../test12/test_out.txt";
     huffman::file_handler file1(t1);
     EXPECT_ANY_THROW(file1.decode(t2));
 }
 
 TEST(exception, no_file_in) {
-    std::string t1 = "test13/test_in.bin";
-    std::string t2 = "test13/test_out.txt";
+    std::string t1 = "../test13/test_in.bin";
+    std::string t2 = "../test13/test_out.txt";
     huffman::file_handler file1(t1);
     EXPECT_ANY_THROW(file1.decode(t2));
 }
 
 TEST(exception, empty_file) {
-    std::string t1 = "test14/test_in.bin";
-    std::string t2 = "test14/test_out.txt";
+    std::string t1 = "../test14/test_in.bin";
+    std::string t2 = "../test14/test_out.txt";
     huffman::file_handler file1(t1);
     EXPECT_ANY_THROW(file1.decode(t2));
 }
